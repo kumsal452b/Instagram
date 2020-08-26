9abd1445d0b2fd73f0370d9fe1aae55bba51b362
kumsal67 <70024440+kumsal67@users.noreply.github.com>
2020-08-26 17:49:57 +0300

    Added firebase server connection elemanrart

---
+++ app/src/main/java/com/example/instagram/Login.java
@@ -0,0 +1,44 @@
+package com.example.instagram;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Login extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+    EditText email,password;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        auth=FirebaseAuth.getInstance();
+        email=findViewById(R.id.loginemail);
+        password=findViewById(R.id.loginpassword);
+
+    }
+    public void signin(View v){
+
+    }
+    public void signup(View v){
+        auth.createUserWithEmailAndPassword(email.getText().toString(),password.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Log.d(Tag,"Created usersuccec")
+                        }
+                    }
+                })
+    }
+}
