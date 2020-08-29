c0eb674125b773ca7731064dbe379826cc5d0189
kumsal67 <70024440+kumsal67@users.noreply.github.com>
2020-08-29 21:56:27 +0300

    added navigation bar

--- app/src/main/java/com/example/instagram/FeedActivity.java
+++ app/src/main/java/com/example/instagram/FeedActivity.java
@@ -1,18 +1,29 @@
 package com.example.instagram;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.app.AlertDialog;
 import android.app.Application;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class FeedActivity extends AppCompatActivity {
 
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle toggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+        drawerLayout=findViewById(R.id.drawer);
+        toggle=new ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 
--- app/src/main/java/com/example/instagram/upload.java
+++ app/src/main/java/com/example/instagram/upload.java
@@ -6,18 +6,11 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 
-import java.util.zip.Inflater;
-
 public class upload extends AppCompatActivity {
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater=new MenuInflater(this);
-        inflater.inflate(R.menu.upload,menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-    
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
