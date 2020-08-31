59784d3f20f073baac8ef4572ddadf15573cf5f3
kumsal67 <70024440+kumsal67@users.noreply.github.com>
2020-08-31 17:07:11 +0300

    Request permissions granted

--- app/src/main/java/com/example/instagram/upload.java
+++ app/src/main/java/com/example/instagram/upload.java
@@ -1,11 +1,16 @@
 package com.example.instagram;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.content.pm.PermissionInfoCompat;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -16,6 +21,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import java.io.IOException;
+
 public class upload extends AppCompatActivity {
 
 
@@ -23,6 +30,7 @@ public class upload extends AppCompatActivity {
     ImageView imageView;
     EditText command;
     Button btn;
+    Bitmap selectedImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,18 +38,23 @@ public class upload extends AppCompatActivity {
         imageView=findViewById(R.id.imageView);
         btn=findViewById(R.id.upload);
         command=findViewById(R.id.commandpost);
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
-                    Uri uri= MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
-                    checkSelfPermission(new String{Manifest.permission.READ_EXTERNAL_STORAGE});
-                }
+
             }
         });
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if (ContextCompat.checkSelfPermission(upload.this,Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+
+                    requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},2);
+                }else{
+                    Intent intent=new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(intent,1);
+                }
 
             }
         });
@@ -50,5 +63,24 @@ public class upload extends AppCompatActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode==1 && grantResults.length>0 && permissions[0]==Manifest.permission.READ_EXTERNAL_STORAGE){
+            Intent intent=new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(intent,1);
+
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==1 && data!=null){
+            try {
+                selectedImage=MediaStore.Images.Media.getBitmap(this.getContentResolver(),MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                imageView.setImageBitmap(selectedImage);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 }
